d5ec0d8bc82eb7e9e32facf9c37ae169cbc5f048
Don Woodford <dwoodford@motivaction.com>
2018-08-29 13:54:27 -0600

    Move and rename files

---
+++ DonsIOCContainer.Tests/ContainerTests.cs
@@ -0,0 +1,53 @@
+﻿using System.Linq;
+using Xunit;
+
+namespace DonsIOCContainer.Tests
+{
+    public class ContainerTests
+    {
+        [Fact]
+        public void Register_ShouldRegisterTypeAsItself()
+        {
+            var container = new IocContainer();
+            container.Register<IDummyInterface, DummyImplementation>();
+
+            var impl = container.Resolve<DummyImplementation>();
+
+            var hello = impl.SayHello();
+
+            Assert.Equal("Hello!", hello);
+        }
+
+        [Fact]
+        public void Register_ShouldRegisterTypeAsInterface()
+        {
+            var container = new IocContainer();
+            container.Register<IDummyInterface, DummyImplementation>();
+
+            var impl = container.Resolve<IDummyInterface>();
+
+            var hello = impl.SayHello();
+
+            Assert.Equal("Hello!", hello);
+        }
+
+        [Fact]
+        public void IsRegistered_ShouldBeTrue_WhenTypeIsRegistered()
+        {
+
+        }
+    }
+
+    public class DummyImplementation : IDummyInterface
+    {
+        public string SayHello()
+        {
+            return "Hello!";
+        }
+    }
+
+    public interface IDummyInterface
+    {
+        string SayHello();
+    }
+}
--- DonsIOCContainer/IocContainer.cs
+++ DonsIOCContainer/IocContainer.cs
@@ -1,45 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using Xunit;
 
-namespace DonsIOCContainer.Tests
+namespace DonsIOCContainer
 {
-    public class ContainerTests
-    {
-        [Fact]
-        public void Register_ShouldRegisterTypeAsItself()
-        {
-            var container = new IocContainer();
-            container.Register<IDummyInterface, DummyImplementation>();
-
-            var impl = container.Resolve<DummyImplementation>();
-
-            var hello = impl.SayHello();
-
-            Assert.Equal("Hello!", hello);
-        }
-
-        [Fact]
-        public void IsRegistered_ShouldBeTrue_WhenTypeIsRegistered()
-        {
-
-        }
-    }
-
-    public class DummyImplementation : IDummyInterface
-    {
-        public string SayHello()
-        {
-            return "Hello!";
-        }
-    }
-
-    public interface IDummyInterface
-    {
-        string SayHello();
-    }
-
     public class IocContainer
     {
         private readonly Dictionary<Type, Type> _registrations = new Dictionary<Type, Type>();
